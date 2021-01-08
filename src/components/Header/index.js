d433b2ec8d0b58206820e4381487c65161f11cd6
unknown <kevin.bernardo64@gmail.com>
2021-01-07 21:22:41 -0300

    header pronto

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Left, Right, UserContainer } from './styles';
+
+// imagens
+import logo from '../../images/logo-header.svg'
+import lupa from '../../images/lupa.svg'
+import conection from '../../images/conection.svg'
+import sino from '../../images/sino.svg'
+import user from '../../images/user.svg'
 
 function Header() {
   return (
       <Container>
-          
+        <Left>
+          <img src={logo} alt="logo-rocketseat" />
+        </Left>
+        <Right>
+           <div id="lupa">
+             <img src={lupa} alt="search" />
+           </div>
+
+           <div id="conexao">
+            <img src={conection} alt="conexão" />
+           </div>
+
+           <div id="sino">
+           <img src={sino} alt="notificações" />
+           </div>
+
+           <UserContainer>
+             <strong>Kevin Alves</strong>
+             <div>
+              <img src={user} alt="userphoto" />
+             </div>
+           </UserContainer>
+
+        </Right>
       </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,105 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display:flex;
+  align-items:center;
+
+  padding:0 80px;
+  
   width:100%;
   height:72px;
   background-color:#202024;
+`;
+
+export const Left = styled.div`
+   display:flex;
+   align-items:center;
+   height:100% ;
+   width:50%;
+`;
+
+export const Right = styled.div`
+   display:flex;
+   align-items:center;
+   justify-content:flex-end;
+
+   height:100% ;
+   width:50%;
+
+   > div{
+       
+        :first-child{
+           margin-right:14px;
+           height:45px;
+            width:45px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            border-radius:5px;
+
+            transition: background-color 200ms;
+            
+            &:hover{
+            background-color:#141316;
+            }
+       }
+
+        :nth-child(2){
+            height:45px;
+            width:45px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            border-radius:5px;
+            margin:0 14px;
+
+            transition: background-color 200ms;
+            
+            &:hover{
+            background-color:#141316;
+            }
+        }
+
+        :nth-child(3){
+            height:45px;
+            width:45px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            border-radius:5px;
+            margin:0 14px;
+
+            transition: background-color 200ms;
+            
+            &:hover{
+            background-color:#141316;
+            }
+        }
+     
+    }
+`;
+
+export const UserContainer = styled.div`
+  display:flex;
+  align-items:center;
+
+  &:hover{
+      background-color:unset;
+  }
+
+  > strong{
+      font-weight:700;
+      color:#E1E1E6;
+      margin-right:16px;
+  }
+
+  > div{
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      height:50px;
+      width:50px;
+      background-color:#E1E1E6;
+      border-radius:50%;
+  }
 `;
